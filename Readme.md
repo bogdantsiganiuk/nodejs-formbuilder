71bb727e3f80e2c8bdb191184cf3edc5b4376d1f
Bogdan Tsiganiuk <bogdan.tsiganiyk@gmail.com>
2020-09-23 02:01:40 +0300

    fixed scriped, fixed readme

---
+++ Readme.md
@@ -0,0 +1,2 @@
+Edit run_all.bat, and set mongolocation to the bin directory of mongo.
+Run "run_all", wait for everything to install and launch.
--- run_all.bat
+++ run_all.bat
@@ -5,6 +5,10 @@ START "" /D %mongolocation% "mongod"
 timeout 2
 START "" /D %mongolocation% "mongo"
 timeout 2
+START "" "install.bat"
+timeout 30
+START "" "%~dp0\client\client-app\install.bat"
+timeout 30
 START "" "server.bat"
 timeout 5
 START "" "%~dp0\client\client-app\client.bat"
